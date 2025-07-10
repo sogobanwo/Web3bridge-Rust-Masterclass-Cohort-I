1b3d0ff1f1eac2b23b94294e0469bf3bfa451967
thebabalola <t.babalolajoseph@gmail.com>
2025-07-10 13:28:44 +0100

    Add Day 4 Task 1 and Task 2 for Babalola Taiwo

---
+++ submissions/week-1/day-4/Babalola-Taiwo_Day4-Tasks/Task-1/src/main.rs
@@ -0,0 +1,40 @@
+enum ItemType {
+    Book,
+    Magazine,
+    Fiction,
+}
+
+struct LibraryItem {
+    quantity: i32,
+    id: i32,
+    item_type: ItemType,
+}
+
+fn display_quantity(item: &LibraryItem) {
+    println!("Quantity: {}", item.quantity);
+}
+
+fn display_id(item: &LibraryItem) {
+    println!("ID Number: {}", item.id);
+}
+
+fn display_item_type(item: &LibraryItem) {
+    let item_type_str = match item.item_type {
+        ItemType::Book => "Book",
+        ItemType::Magazine => "Magazine",
+        ItemType::Fiction => "Fiction",
+    };
+    println!("Item Type: {}", item_type_str);
+}
+
+fn main() {
+    let my_book = LibraryItem {
+        quantity: 10,
+        id: 101,
+        item_type: ItemType::Book,
+    };
+
+    display_quantity(&my_book);
+    display_id(&my_book);
+    display_item_type(&my_book);
+}
