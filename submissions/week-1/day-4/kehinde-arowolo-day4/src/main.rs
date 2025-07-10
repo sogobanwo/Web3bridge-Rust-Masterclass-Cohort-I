bed148c058c1a7f4c0485e084b38de794a7bd214
mac <arowolokehinde321@gmail.com>
2025-07-10 14:12:38 +0100

    library item management

--- submissions/week-1/day-4/kehinde-arowolo-day4/src/main.rs
+++ submissions/week-1/day-4/kehinde-arowolo-day4/src/main.rs
@@ -23,3 +23,44 @@ println!("ID: {}", item.id);
 fn display_item_type(item: &LibraryItem) {
     println!("Item Type: {:?}", item.item_type);
 }
+
+#[derive(Debug)]
+enum BoxColor {
+    Red,
+    Blue,
+    Green,
+    Brown,
+    White,
+}
+
+struct ShippingBox {
+    length: f64,
+    width: f64,
+    height: f64,
+    weight: f64,
+    color: BoxColor,
+}
+
+impl ShippingBox {
+    fn new(length: f64, width: f64, height: f64, weight: f64, color: BoxColor) -> Self {
+        ShippingBox {
+            length,
+            width,
+            height,
+            weight,
+            color,
+        }
+    }
+
+    fn print_characteristics(&self) {
+        println!("Box Characteristics:");
+        println!("  Dimensions: {}L x {}W x {}H", self.length, self.width, self.height);
+        println!("  Weight: {} kg", self.weight);
+        println!("  Color: {:?}", self.color);
+    }
+}
+
+fn main() {
+    let my_box = ShippingBox::new(10.5, 8.0, 6.0, 2.3, BoxColor::Brown);
+    my_box.print_characteristics();
+}
