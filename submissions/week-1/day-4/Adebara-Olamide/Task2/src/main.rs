8e9ab4b0c5c584449ee1b542aec30e7defe669a9
holamite <olamideadebara@gmail.com>
2025-07-10 14:14:05 +0100

    fix: the dimension properly

--- submissions/week-1/day-4/Adebara-Olamide/Task2/src/main.rs
+++ submissions/week-1/day-4/Adebara-Olamide/Task2/src/main.rs
@@ -5,7 +5,7 @@ enum boxColor {
     Yellow,
 }
 struct ShippingBox {
-    dimension: f64,
+    dimension: (f64, f64, f64),
     width: f64,
     box_color: boxColor,
 }
@@ -13,14 +13,14 @@ struct ShippingBox {
 impl ShippingBox {
     fn new(dimension: f64, width: f64, box_color: boxColor) -> Self {
         Self {
-            dimension,
+            dimension: (dimension, dimension, dimension),
             width,
             box_color,
         }
     }
 
     fn display_shipping_box(&self) {
-        println!("Dimension: {}", self.dimension);
+        println!("Dimension: {}, {}, {}", self.dimension.0, self.dimension.1, self.dimension.2);
         println!("Width: {}", self.width);
         match self.box_color {
             boxColor::Red => println!("Color: Red"),
@@ -31,9 +31,9 @@ impl ShippingBox {
     }
 }
 fn main() {
-    let box1 = ShippingBox::new(10.0, 5.0, boxColor::Red);
-    let box2 = ShippingBox::new(15.0, 7.5, boxColor::Blue);
-    let box3 = ShippingBox::new(20.0, 10.0, boxColor::Green);
+    let box1 = ShippingBox::new((10.0, 10.0, 10.0), 5.0, boxColor::Red);
+    let box2 = ShippingBox::new((15.0, 11.0, 10.0), 7.5, boxColor::Blue);
+    let box3 = ShippingBox::new((10.0, 30.0, 10.0), 10.0, boxColor::Green);
 
     box1.display_shipping_box();
     box2.display_shipping_box();
