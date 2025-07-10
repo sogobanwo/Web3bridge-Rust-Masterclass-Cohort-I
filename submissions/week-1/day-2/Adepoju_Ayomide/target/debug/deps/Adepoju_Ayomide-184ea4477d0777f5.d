696878d41cb5db02aa7ffc747da7a98b8d434077
adepoju2006 <adepojuayomide2006@gmail.com>
2025-07-10 12:12:09 +0100

    Adepoju-Ayomide Day-2

--- submissions/week-1/day-2/Adepoju_Ayomide/target/debug/deps/Adepoju_Ayomide-184ea4477d0777f5.d
+++
@@ -1,5 +0,0 @@
-/home/fade/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Adepoju_Ayomide/target/debug/deps/Adepoju_Ayomide-184ea4477d0777f5.d: src/main.rs
-
-/home/fade/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-2/Adepoju_Ayomide/target/debug/deps/Adepoju_Ayomide-184ea4477d0777f5: src/main.rs
-
-src/main.rs:
