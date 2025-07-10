cc58fab8d3ab5c480f88bc73477fb31ca80db590
mac <arowolokehinde321@gmail.com>
2025-07-10 13:36:06 +0100

    Day 4 tasks

---
+++ submissions/week-1/day-4/kehinde-arowolo-day4/src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+enum ItemType {
+    Book,
+    Magazine,
+    Fiction,
+}
+
+struct LibraryItem {
+    quantity: i32,
+    id: i32,
+    item_type: ItemType,
+}
+
+fn display_quantity(item: &LibraryItem) {
+    println!("Quantity: {}", item.quantity);
+}
+
+fn display_id(item: &LibraryItem) {
+    println!("ID: {}", item.id);
+}
+
+fn display_item_type(item: &LibraryItem) {
+    println!("Item Type: {:?}", item.item_type);
+}
+
+fn main() {
+    println!("Hello, world!");
+}
