07c0d390155a340f12fa87ec58f73ddbb9cf1335
thebabalola <t.babalolajoseph@gmail.com>
2025-07-12 09:33:44 +0100

    Add Week 1, Day 5 task for Babalola Taiwos Todo and Enums Task

---
+++ submissions/week-1/day-5/Babalola-Taiwo_Day5-Tasks1-2/Task-2/src/main.rs
@@ -0,0 +1,58 @@
+
+fn main() {
+    let fiction_book = Book::Fiction {
+        title: String::from("The Alchemist"),
+        author: String::from("Paulo Coelho"),
+        price: 4000.00,
+    };
+
+    let magazine = Book::Magazine {
+        title: String::from("Science Monthly"),
+        author: String::from("Dr. Emeka Okafor"),
+        price: 2000.00,
+    };
+
+    let sci_fi_book = Book::SciFi {
+        title: String::from("Neon Horizon"),
+        price: 3500.00,
+    };
+
+    let library = vec![fiction_book, magazine, sci_fi_book];
+
+    print_book_details(library);
+
+    println!();
+}
+
+
+enum Book {
+    Fiction { title: String, author: String, price: f32 },
+    Magazine { title: String, author: String, price: f32 },
+    SciFi { title: String, price: f32 },
+}
+
+fn print_book_details(books: Vec<Book>) {
+    for book in books {
+        match book {
+            Book::Fiction { title, author, price } => {
+                println!(
+                    "📘 Fiction Book: \"{}\" by {} - ₦{:.2}",
+                    title, author, price
+                );
+            }
+            Book::Magazine { title, author, price } => {
+                println!(
+                    "📰 Magazine: \"{}\" by {} - ₦{:.2}",
+                    title, author, price
+                );
+            }
+            Book::SciFi { title, price } => {
+                println!(
+                    "👽 Sci-Fi Book: \"{}\" - ₦{:.2}",
+                    title, price
+                );
+            }
+        }
+    }
+}
+
