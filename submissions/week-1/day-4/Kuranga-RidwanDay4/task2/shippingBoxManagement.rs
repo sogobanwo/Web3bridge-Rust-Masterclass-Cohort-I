398908ccd008ec896f13f0afd023c12cb7d40a18
KurangaRidwan <kurangaridwan96@gmail.com>
2025-07-13 12:46:08 +0100

    New Changes

--- submissions/week-1/day-4/Kuranga-RidwanDay4/task2/shippingBoxManagement.rs
+++ submissions/week-1/day-4/Kuranga-RidwanDay4/task2/shippingBoxManagement.rs
@@ -1,10 +1,11 @@
-fn main(){
-    enum BoxColor {
+// Declare enum outside of main
+enum BoxColor {
     Red,
     Blue,
     Green,
 }
 
+// Declare struct outside of main
 struct ShippingBox {
     length: f32,
     width: f32,
@@ -13,6 +14,7 @@ struct ShippingBox {
     color: BoxColor,
 }
 
+// Implement methods outside of main
 impl ShippingBox {
     fn new(length: f32, width: f32, height: f32, weight: f32, color: BoxColor) -> ShippingBox {
         ShippingBox {
@@ -36,4 +38,8 @@ impl ShippingBox {
     }
 }
 
+fn main() {
+    // Now you can create an instance and use it inside main
+    let box1 = ShippingBox::new(30.0, 20.0, 15.0, 2.5, BoxColor::Blue);
+    box1.print_characteristics();
 }
