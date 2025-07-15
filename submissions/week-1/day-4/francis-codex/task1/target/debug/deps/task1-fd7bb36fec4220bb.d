a115872ba32d597dd79cf2bc68d042b3d65bbf6d
francis-codex <francislogo2219@gmail.com>
2025-07-15 13:34:43 +0100

    remove target folder from repository

--- submissions/week-1/day-4/francis-codex/task1/target/debug/deps/task1-fd7bb36fec4220bb.d
+++
@@ -1,5 +0,0 @@
-/home/dev/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-4/francis-codex/task1/target/debug/deps/task1-fd7bb36fec4220bb.d: src/main.rs
-
-/home/dev/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-1/day-4/francis-codex/task1/target/debug/deps/task1-fd7bb36fec4220bb: src/main.rs
-
-src/main.rs:
