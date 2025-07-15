d0bd0318204a5938473d436a845abde870d7ace0
James Osunkiyesi <jaydhales@gmail.com>
2025-07-15 12:05:18 +0100

    Add class management system with student registration and course management functions

---
+++ submissions/week-2/day-2/James_Osunkiyesi_Class_Management/src/lib.rs
@@ -0,0 +1,103 @@
+// A class management system that has the name of the student, grade, enum that tracks if student is active or not.
+//
+// Have the following functions:
+// - Function to register student
+// - Edit
+// - Update
+// - Delete functions
+// - View function
+
+use std::collections::HashMap;
+
+enum StudentStatus {
+    ACTIVE,
+    EXPELLED,
+    SUSPENDED,
+    INACTIVE,
+}
+
+struct Subject {
+    name: String,
+    grade: u8,
+}
+
+struct Student {
+    name: String,
+    status: StudentStatus,
+    grades: HashMap<String, u8>,
+}
+
+impl Student {
+    fn register(name: String) -> Student {
+        Student {
+            name,
+            status: StudentStatus::ACTIVE,
+            grades: HashMap::new(),
+        }
+    }
+
+    fn register_course(&mut self, subject: &str) {
+        if self.grades.contains_key(subject) {
+            panic!("Course already registered");
+        }
+        self.grades.insert(subject.to_string(), 0);
+    }
+
+    fn add_score(&mut self, subject: &str, score: u8) {
+        if self.grades.contains_key(subject) {
+            self.grades
+                .entry(subject.to_string())
+                .and_modify(|e| *e = score);
+        } else {
+            panic!("Course not registered");
+        }
+    }
+
+    fn remove_course(&mut self, subject: &str) {
+        if self.grades.contains_key(subject) {
+            self.grades.remove(subject);
+        } else {
+            panic!("Course not registered");
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::cmp::PartialEq;
+    use super::*;
+
+    #[test]
+    fn test_register_student() {
+        let student_one = Student::register(String::from("James Osunkiyesi"));
+
+        assert!(&student_one.name.eq(("James Osunkiyesi")));
+    }
+
+    #[test]
+    fn test_register_course() {
+        let mut student_one = Student::register(String::from("James Osunkiyesi"));
+
+        student_one.register_course("Math");
+
+        assert!(student_one.grades.contains_key("Math"));
+    }
+
+    #[test]
+    fn test_add_score() {
+        let mut student_one = Student::register(String::from("James Osunkiyesi"));
+        student_one.register_course("Math");
+
+        student_one.add_score("Math", 60);
+        assert_eq!(*student_one.grades.get("Math").unwrap(), 60);
+    }
+
+    #[test]
+    fn test_remove_course() {
+        let mut student_one = Student::register(String::from("James Osunkiyesi"));
+        student_one.register_course("Math");
+
+        student_one.remove_course("Math");
+        assert!(!student_one.grades.contains_key("Math"));
+    }
+}
