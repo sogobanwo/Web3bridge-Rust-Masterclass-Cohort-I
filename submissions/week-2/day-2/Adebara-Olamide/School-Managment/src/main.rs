d605e0d9989f22a1e8ac1f74a56ac0200769db86
holamite <olamideadebara@gmail.com>
2025-07-15 11:36:48 +0100

    Update: implementation completed

--- submissions/week-2/day-2/Adebara-Olamide/School-Managment/src/main.rs
+++ submissions/week-2/day-2/Adebara-Olamide/School-Managment/src/main.rs
@@ -1,3 +1,50 @@
+struct Student{
+    name: String,
+    grade, u8,
+    status: Status  
+}
+
+enum Status{
+    active,
+    inactive,
+}
+
+struct School{
+    students: Vec<Student>,
+}
+
+impl School{
+    fn new() -> School{
+        School {
+            students: Vec::new(),
+        }
+    },
+
+    fn add_student(&mut self, name: &str, grade: u8, status: Status) {
+        let student = Student {
+            name: String::from(name),
+            grade,
+            status,
+        };
+        self.students.push(student);
+    }
+
+    fn edit_student(&mut self, name: &str, new_name: &str, new_grade: u8, new_status: Status){
+        self.students.iter_mut().find(|student| student.name == name ){
+            student.grade = new_grade;
+            student.status = new_status;
+        };
+    }
+
+    fn remove_student(&mut self, name:&str){
+        self.students.retain(|student| student.name != name);
+    }
+
+    fn view_students(&self){
+        self.students
+    }
+
+}
 fn main() {
     println!("Hello, world!");
 }
