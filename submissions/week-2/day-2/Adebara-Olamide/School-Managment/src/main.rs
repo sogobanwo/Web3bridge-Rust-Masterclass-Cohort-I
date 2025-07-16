c314a879ce6628b7be742258bd2d3779e5989e53
holamite <olamideadebara@gmail.com>
2025-07-16 15:07:48 +0100

    Classwork

--- submissions/week-2/day-2/Adebara-Olamide/School-Managment/src/main.rs
+++ submissions/week-2/day-2/Adebara-Olamide/School-Managment/src/main.rs
@@ -1,9 +1,11 @@
 struct Student {
+    id: u8,
     name: String,
     grade: u8,
     status: Status,
 }
 
+#[derive(Debug, PartialEq)]
 enum Status {
     Active,
     InActive,
@@ -11,28 +13,42 @@ enum Status {
 
 struct School {
     students: Vec<Student>,
+    next_id: u8,
 }
 
 impl School {
     fn new() -> School {
         School {
             students: Vec::new(),
+            next_id: 1,
         }
     }
 
     fn add_student(&mut self, name: &str, grade: u8, status: Status) {
+        let id = self.next_id;
         let student = Student {
+            id,
             name: String::from(name),
             grade,
             status,
         };
+        self.next_id += 1;
         self.students.push(student);
     }
 
-    fn edit_student(&mut self, name: &str, new_name: &str, new_grade: u8, new_status: Status) {
-        if let Some(student) = self.students.iter_mut().find(|s| s.name == name) {
+    fn edit_student(&mut self, id: u8, new_name: &str, new_grade: u8, new_status: Status) {
+        if let Some(student) = self.students.iter_mut().find(|s| s.id == id) {
             student.name = new_name.to_string();
             student.grade = new_grade;
+            student.status = new_status;
+            println!("Student edited!");
+        } else {
+            println!("Student not found!");
+        }
+    }
+
+    fn edit_student_status(&mut self, id: u8, new_status: Status) {
+        if let Some(student) = self.students.iter_mut().find(|s| s.id == id) {
             student.status = new_status;
             println!("Student edited!");
         } else {
@@ -44,7 +60,7 @@ impl School {
         self.students.retain(|student| student.name != name);
     }
 
-    fn view_students(&self, index: usize) -> &Student {
+    fn get_students(&self, index: usize) -> &Student {
         self.students.get(index).expect("Student not found")
     }
 }
@@ -66,7 +82,9 @@ mod tests {
     fn test_edit_student() {
         let mut school = School::new();
         school.add_student("Adebara Olamide", 10, Status::Active);
-        school.edit_student("Adebara Olamide", "Adebara Holamite", 75, Status::Active);
+        school.add_student("Adebara John", 20, Status::Active);
+        school.add_student("Adebara Marvellous", 10, Status::Active);
+        school.edit_student(1, "Adebara Holamite", 75, Status::Active);
         assert_eq!(school.students[0].name, "Adebara Holamite");
         assert_eq!(school.students[0].grade, 75);
     }
@@ -84,8 +102,19 @@ mod tests {
     fn test_view_students() {
         let mut school = School::new();
         school.add_student("Adebara Olamide", 10, Status::Active);
-        school.view_students(1);
+        school.get_students(1);
         assert_eq!(school.students[0].name, "Adebara Olamide");
         assert_eq!(school.students[0].grade, 10);
+    }
+
+    #[test]
+    fn test_edit_student_status() {
+        let mut school = School::new();
+        school.add_student("Adebara Olamide", 10, Status::Active);
+        // fn edit_student_status(&mut self, id: u8, new_status: Status)
+        assert_eq!(school.students[0].status, Status::Active);
+        school.edit_student_status(1, Status::InActive);
+        // school.get_students(1);
+        assert_eq!(school.students[0].status, Status::InActive)
     }
 }
---
+++ submissions/week-2/day-2/Adebara-Olamide/School-Managment/target/debug/deps/School_Managment-638a8ed5134364dd.d
@@ -0,0 +1,5 @@
+/Users/pc/Desktop/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/Adebara-Olamide/School-Managment/target/debug/deps/School_Managment-638a8ed5134364dd.d: src/main.rs
+
+/Users/pc/Desktop/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/Adebara-Olamide/School-Managment/target/debug/deps/School_Managment-638a8ed5134364dd: src/main.rs
+
+src/main.rs:
