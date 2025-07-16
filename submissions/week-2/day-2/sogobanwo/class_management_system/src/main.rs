2e0b4b5161f7aa223df274ec675c20a5f9d00cab
sogobanwo <sogobanwo@gmail,com>
2025-07-16 12:42:40 +0100

    feat: added student id

--- submissions/week-2/day-2/sogobanwo/class_management_system/src/main.rs
+++ submissions/week-2/day-2/sogobanwo/class_management_system/src/main.rs
@@ -1,59 +1,87 @@
 fn main() {
-    println!("Hello, world!");
+    let mut class = ClassOfStudents::init_class();
+    class.create_student("Sogo".to_string(), 100.0, ActivesStatus::NotActive);
+    class.create_student("Shogzymamadowski".to_string(), 100.0, ActivesStatus::Active);
+    class.update_active_status(0);
+    class.update_active_status(1);
 }
+
 #[derive(Debug, Clone)]
-enum ActivesStatus{
+enum ActivesStatus {
     Active,
     NotActive,
 }
 
 #[derive(Debug, Clone)]
 struct Student {
+    id: usize,
     name: String,
     grade: f64,
-    status:ActivesStatus,
+    status: ActivesStatus,
 }
 
 #[derive(Debug, Clone)]
-struct ClassOfStudents{
+struct ClassOfStudents {
     class_students: Vec<Student>,
 }
 
 impl ClassOfStudents {
-
-    fn init_class ()-> ClassOfStudents{
-        ClassOfStudents{
-            class_students: Vec::new()
+    fn init_class() -> ClassOfStudents {
+        ClassOfStudents {
+            class_students: Vec::new(),
         }
     }
 
-    fn create_student (&mut self, student: Student) {
-        self.class_students.push(student);
+    fn create_student(&mut self, name: String, grade: f64,  status: ActivesStatus) {
+        let new_student = Student {
+            id: self.class_students.len(),
+            name,
+            grade,
+            status
+        };
+        self.class_students.push(new_student);
     }
 
-    fn get_all_students (&self) -> Vec<Student>{
+    fn get_all_students(&self) -> Vec<Student> {
         self.class_students.to_vec()
     }
 
-    fn get_each_student (&self, index: usize) -> Student{
-        if self.class_students.len() > index {
-            self.class_students[index].clone()
+    fn get_each_student(&self, id: usize) -> Student {
+        if self.class_students.len() > id {
+            self.class_students[id].clone()
         } else {
             panic!("student does not exist");
         }
     }
 
-    fn update_student (&mut self, index:usize, student: Student){
-         if self.class_students.len() > index {
-            self.class_students[index] = student;
+    fn update_student(&mut self, id: usize, student: Student) {
+        if self.class_students.len() > id {
+            self.class_students[id] = student;
         } else {
             panic!("student does not exist");
         }
     }
 
-    fn delete_student (&mut self, index:usize){
-        if self.class_students.len() > index {
-            self.class_students.remove(index);
+    fn update_active_status(&mut self, id: usize) -> Student {
+        if self.class_students.len() > id {
+            let student_to_update = self.class_students[id].clone();
+            let updated_student = Student {
+                status: match student_to_update.status {
+                    ActivesStatus::Active => ActivesStatus::NotActive,
+                    ActivesStatus::NotActive => ActivesStatus::Active,
+                },
+                ..student_to_update
+            };
+            println!("Updated student is {:?}", &updated_student);
+            updated_student
+        } else {
+            panic!("student does not exist");
+        }
+    }
+
+    fn delete_student(&mut self, id: usize) {
+        if self.class_students.len() > id {
+            self.class_students.remove(id);
         } else {
             panic!("student does not exist");
         }
@@ -68,17 +96,10 @@ mod tests {
     fn test_create_student() {
         let mut class = ClassOfStudents::init_class();
         assert!(class.class_students.len() == 0);
-
-        let student = Student {
-            name: "Sogo".to_string(),
-            grade: 4.5,
-            status:ActivesStatus::Active
-        };
-
-        class.create_student(student);
+        class.create_student("Sogo".to_string(), 100.0, ActivesStatus::NotActive);
         assert!(class.class_students.len() == 1);
     }
 
-    
-  
+    #[test]
+    fn test_get_all_students() {}
 }
