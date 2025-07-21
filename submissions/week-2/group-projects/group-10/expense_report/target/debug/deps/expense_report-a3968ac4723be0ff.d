19ace5fc247414aaba71e85a0ac567886498b62a
Emmanuel Aroso <57766083+emarc99@users.noreply.github.com>
2025-07-21 09:36:24 +0100

    chore: del target

--- submissions/week-2/group-projects/group-10/expense_report/target/debug/deps/expense_report-a3968ac4723be0ff.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/LENOVO/Documents/onlydust/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-10/expense_report/target/debug/deps/expense_report-a3968ac4723be0ff: src/main.rs
-
-/mnt/c/Users/LENOVO/Documents/onlydust/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-10/expense_report/target/debug/deps/expense_report-a3968ac4723be0ff.d: src/main.rs
-
-src/main.rs:
--- submissions/week-2/group-projects/group-10/expense_report/target/debug/deps/expense_report-c42469b06ebb91bb.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/LENOVO/Documents/onlydust/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-10/expense_report/target/debug/deps/expense_report-c42469b06ebb91bb: src/main.rs
-
-/mnt/c/Users/LENOVO/Documents/onlydust/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/group-10/expense_report/target/debug/deps/expense_report-c42469b06ebb91bb.d: src/main.rs
-
-src/main.rs:
