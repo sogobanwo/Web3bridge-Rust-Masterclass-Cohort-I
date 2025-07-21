59dd067f17d36c3b49f14f410db188356c6f1b1b
francis-codex <francislogo2219@gmail.com>
2025-07-21 09:36:58 +0100

    group23 task: sales lead tracker completed by francis-codex, uibeanuka, and Uchechukwu-Ekezie

--- submissions/week-2/day-3/francis-codex/task1/src/lib.rs
+++ submissions/week-2/day-3/francis-codex/task1/src/lib.rs
@@ -1,20 +1,3 @@
-#[derive(Debug)]
-pub enum Employees {
-    MediaTeam, 
-    IT,
-    Managers, 
-    SocialMedia,
-    TechnicianSupervisors,
-    KitchenStaff,
-}
-
-#[derive(Debug)]
-pub struct Employee {
-    pub position: Employees,
-    pub is_employed: bool, 
-}
-
-
 pub fn check_building_access(employee: &Employee) -> Result <(), String> {
 
     if !employee.is_employed {
