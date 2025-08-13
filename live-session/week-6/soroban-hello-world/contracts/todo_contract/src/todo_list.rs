84e645b550fec2f6e302e0c600e131b8d31f8ed5
Josh_dfG <agboolajoshua161@gmail.com>
2025-08-13 16:06:46 +0100

    feat: update live session and submission folder

--- live-session/week-6/soroban-hello-world/contracts/todo_contract/src/todo_list.rs
+++ live-session/week-6/soroban-hello-world/contracts/todo_contract/src/todo_list.rs
@@ -1,12 +1,15 @@
-use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Env, String, Symbol, Vec};
+use soroban_sdk::{
+    contract, contractimpl, contracttype, symbol_short, xdr::SorobanCredentials, Env, String,
+    Symbol, Vec,
+};
 
 #[contracttype]
 #[derive(Debug, PartialEq, Eq, Clone)]
-struct Todo {
-    id: u32,
-    title: String,
-    description: String,
-    status: bool,
+pub struct Todo {
+    pub id: u32,
+    pub title: String,
+    pub description: String,
+    pub status: bool,
 }
 
 const TODOS: Symbol = symbol_short!("TOD0S");
@@ -39,6 +42,67 @@ impl Todolist {
         env.storage().persistent().set(&NEXT_ID, &current_id);
 
         todo
+    }
+
+    pub fn update_todo(env: Env, id: u32, title: String, description: String) -> bool {
+        let mut todos = Self::get_todos(&env);
+        for i in 0..todos.len() {
+            let mut updated = todos.get(i).unwrap();
+            if updated.id == id {
+                updated.title = title;
+                updated.description = description;
+                todos.set(i, updated);
+                env.storage().persistent().set(&TODOS, &todos);
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn complete_todo(env: Env, id: u32) -> bool {
+        let mut todos = Self::get_todos(&env);
+
+        for i in 0..todos.len() {
+            if let Some(mut todo) = todos.get(i) {
+                if todo.id == id {
+                    todo.status = !todo.status;
+                    todos.set(i, todo);
+                    env.storage().persistent().set(&TODOS, &todos);
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    pub fn delete_todo(env: Env, id: u32) -> bool {
+        let mut todos = Self::get_todos(&env);
+
+        if let Some(todo) = todos.iter().position(|i| i.id == id) {
+            todos.remove(todo as u32);
+            env.storage().persistent().set(&TODOS, &todos);
+            return true;
+        }
+
+        false
+    }
+
+    pub fn update_todo2(env: Env, id: u32, title: String, description: String) -> bool {
+        let mut todos = Self::get_todos(&env);
+
+        for i in 0..todos.len() {
+            if let Some(mut todo) = todos.get(i) {
+                if todo.id == id {
+                    todo.title = title;
+                    todo.description = description;
+                    todos.set(i, todo);
+                    env.storage().persistent().set(&TODOS, &todos);
+
+                    return true;
+                }
+            }
+        }
+        false
     }
 
     pub fn get_todos(env: &Env) -> Vec<Todo> {
